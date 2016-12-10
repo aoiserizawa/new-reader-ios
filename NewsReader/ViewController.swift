a8186609f069ed3a5aba266ce435078cab2e3f58
Alvin Joseph Valdez <alvin@directworksmedia.com>
2016-12-11 02:52:59 +0800

    added alamofire via pod to shorten the code

--- NewsReader/ViewController.swift
+++ NewsReader/ViewController.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     
     @IBOutlet weak var tableViewArticles: UITableView!
-    
     var articles: [Article]? = []
+    var urlString = "https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=4bdf758eac5f4bba9fd63190fb286ee2"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,45 +24,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func fetchArticles(){
-        let urlRequest = URLRequest(url: URL(string: "https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=4bdf758eac5f4bba9fd63190fb286ee2")!)
+        Alamofire.request(urlString).responseJSON(completionHandler: {
+            response in
+            self.parseData(JSONData: response.data!)
+        })
+    }
+    
+    
+    func parseData(JSONData: Data){
+        self.articles = [Article]()
         
-        let task = URLSession.shared.dataTask(with: urlRequest){ (data, response, error) in
-            if error != nil{
-                print(error)
-                return
-            }
-            
-            self.articles = [Article]()
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
-                if let articlesFromJson = json["articles"] as? [[String: AnyObject]]{
+        do {
+            let json = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! [String: AnyObject]
+            if let articlesFromJson = json["articles"] as? [[String: AnyObject]]{
+                
+                for articleFromJson in articlesFromJson{
+                    let article = Article()
                     
-                    for articleFromJson in articlesFromJson{
-                        let article = Article()
+                    if let title = articleFromJson["title"] as? String, let author = articleFromJson["author"] as? String, let desc = articleFromJson["description"] as? String, let url = articleFromJson["url"] as? String, let urlToImage = articleFromJson["urlToImage"] as? String{
                         
-                        if let title = articleFromJson["title"] as? String, let author = articleFromJson["author"] as? String, let desc = articleFromJson["description"] as? String, let url = articleFromJson["url"] as? String, let urlToImage = articleFromJson["urlToImage"] as? String{
-                            
-                            article.author = author
-                            article.desc = desc
-                            article.headline = title
-                            article.url = url
-                            article.imgUrl = urlToImage
-                        }
-                        self.articles?.append(article)
+                        article.author = author
+                        article.desc = desc
+                        article.headline = title
+                        article.url = url
+                        article.imgUrl = urlToImage
                     }
-                    
-                }
-                DispatchQueue.main.async {
-                    self.tableViewArticles.reloadData()
+                    self.articles?.append(article)
                 }
                 
             }
-            catch let error{
-                print(error)
+            DispatchQueue.main.async {
+                self.tableViewArticles.reloadData()
             }
+            
         }
-        
-        task.resume()
+        catch let error{
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
